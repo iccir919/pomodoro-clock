a33b4ef621c418d8c3915ad6052c1cf956fd3b6e
Neil Ricci <iccir919@gmail.com>
2018-07-08 13:04:35 -0700

    Button styling complete

--- Cardi-B-Noise-Machine/src/DrumPad.js
+++ Cardi-B-Noise-Machine/src/DrumPad.js
@@ -1,10 +1,37 @@
 import React, { Component } from "react";
 
+const activeStyle = {
+  backgroundColor: "orange",
+  boxShadow: "0 3px orange",
+  height: 77,
+  marginTop: 13
+};
+
+const inactiveStyle = {
+  backgroundColor: "grey",
+  marginTop: 10,
+  boxShadow: "3px 3px 5px black"
+};
+
 class DrumPad extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      padStyle: inactiveStyle
+    };
     this.playSound = this.playSound.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.activatePad = this.activatePad.bind(this);
+  }
+
+  activatePad() {
+    this.state.padStyle.backgroundColor === "orange"
+      ? this.setState({
+          padStyle: inactiveStyle
+        })
+      : this.setState({
+          padStyle: activeStyle
+        });
   }
 
   componentDidMount() {
@@ -24,11 +51,18 @@ class DrumPad extends React.Component {
     const sound = document.getElementById(this.props.keyTrigger);
     sound.currentTime = 0;
     sound.play();
+    this.activatePad();
+    setTimeout(() => this.activatePad(), 100);
   }
 
   render() {
     return (
-      <div id={this.props.clipId} onClick={this.playSound} className="drum-pad">
+      <div
+        id={this.props.clipId}
+        onClick={this.playSound}
+        className="drum-pad"
+        style={this.state.padStyle}
+      >
         <audio
           className="clip"
           id={this.props.keyTrigger}
