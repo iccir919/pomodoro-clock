ebb61c091b6227d6c33361eb3caef1d3b40d3fa6
Neil Ricci <iccir919@gmail.com>
2018-07-21 20:07:28 -0700

    Boilerplate setup

---
+++ Pomodoro-Clock/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Testing... 1, 2, 3</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
