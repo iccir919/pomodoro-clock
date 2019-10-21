3c33afb3ee1b7e29f11254134f7e5b6ceab3054a
Neil Ricci <iccir919@gmail.com>
2019-10-21 09:13:27 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iccir919/FCC-Frontend-Libraries-Projects/pomodoro-clock/precache-manifest.8284b34d2cee3af81fb355199c875661.js"
+  "/iccir919/FCC-Frontend-Libraries-Projects/precache-manifest.e8e1cd881ca6c08e9a65e8e6a698952e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iccir919/FCC-Frontend-Libraries-Projects/pomodoro-clock/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/iccir919/FCC-Frontend-Libraries-Projects/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
