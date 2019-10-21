b3cf08c743580f77d8f5fda8bbcd609a1a424522
Neil Ricci <iccir919@gmail.com>
2019-10-21 09:52:13 -0700

    Updates

---
+++ precache-manifest.395ea7cfc996a2e4ec2b26709b966dbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac95838847d4dc39292c9a9f0da33a6b",
+    "url": "/fcc-frontend-libraries-projects/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "2ce46420a83282a62847",
+    "url": "/fcc-frontend-libraries-projects/pomodoro-clock/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "5cd13804a19ec876b653",
+    "url": "/fcc-frontend-libraries-projects/pomodoro-clock/static/js/2.fd1cf378.chunk.js"
+  },
+  {
+    "revision": "2ce46420a83282a62847",
+    "url": "/fcc-frontend-libraries-projects/pomodoro-clock/static/js/main.80c14e40.chunk.js"
+  },
+  {
+    "revision": "b5dc4a4ae3f06b87d20c",
+    "url": "/fcc-frontend-libraries-projects/pomodoro-clock/static/js/runtime-main.d3016e6a.js"
+  }
+]);
