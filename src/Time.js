123c00bff446ff08897d4a4ba59a6194cd80631c
Neil Ricci <iccir919@gmail.com>
2019-10-28 19:02:29 -0700

    Add current time to HTML title

--- src/Time.js
+++ src/Time.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 function Time(props) {
+
   let minutes = Math.floor(props.remainingTime / 60);
   let seconds = props.remainingTime % 60;
 
   if (minutes < 10) minutes = "0" + minutes;
   if (seconds < 10) seconds = "0" + seconds;
+  document.title = `${minutes}:${seconds} - Pomodoro Clock`;
   return (
     <div>
       <h2 id="timer-label">{props.isSession ? "session" : "break"}</h2>
