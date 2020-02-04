83e2e5b26361237cf43ccabeda87b549c4b6bf2c
Neil Ricci <iccir919@gmail.com>
2020-02-03 19:20:51 -0800

    First test with enzyme

--- src/PomodoroClock.test.js
+++ src/PomodoroClock.test.js
@@ -1,9 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PomodoroClock from "./PomodoroClock";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import PomodoroClock from "./PomodoroClock";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<PomodoroClock />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it("renders without crashing", () => {
+//   const div = document.createElement("div");
+//   ReactDOM.render(<PomodoroClock />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import PomodoroClock from './PomodoroClock';
+it('renders without crashing', () => {
+  shallow(<PomodoroClock />);
 });
